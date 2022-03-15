c6ed0c2bcddcc037e932d21c7085eefd466e768d
Ujala-355 <98416834+Ujala-355@users.noreply.github.com>
2022-03-15 17:38:27 +0530

    Update task8.py

--- task8.py
+++ task8.py
@@ -9,10 +9,6 @@ def caching_movie_details():
           if os.path.exists(p):
                pass
           else:
-               # create_file=open(("/home/admin123/Task8"+i["Name"])+".text","w")
-               # url=requests.get(i["Url"])
-               # create_file1=create_file.write(url.text)
-               # create_file.close()
                with open(("/home/admin123/Task8"+i["Name"])+".text","w") as f:
                     url=requests.get(i["Url"])
                     f=f.write(url.text)               
